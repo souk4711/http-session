830fd6433f10347aec21d72a8d65a04cea775807
John Doe <johndoe@example.com>
2023-10-07 10:37:11 +0800

    chore: Add test-novcr workflow on CI

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,11 +1,14 @@
-require "vcr"
+unless ENV["NOVCR"]
+  require "vcr"
+  require "webmock/rspec"
 
-VCR.configure do |config|
-  config.cassette_library_dir = RSPEC_ROOT.join("fixtures/vcr_cassettes")
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
-  config.preserve_exact_body_bytes do |http_message|
-    http_message.body.encoding.name == Encoding::ASCII_8BIT ||
-      !http_message.body.valid_encoding?
+  VCR.configure do |config|
+    config.cassette_library_dir = RSPEC_ROOT.join("fixtures/vcr_cassettes")
+    config.hook_into :webmock
+    config.configure_rspec_metadata!
+    config.preserve_exact_body_bytes do |http_message|
+      http_message.body.encoding.name == Encoding::ASCII_8BIT ||
+        !http_message.body.valid_encoding?
+    end
   end
 end
